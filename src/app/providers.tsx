c08c13ebbcbfa8088e3257dc4b029a267493d9e7
aiwon0035 <OsakoSeiho@gmail.com>
2024-02-28 17:25:43 +0900

    dnd-kitで並び替えを追加

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+// app/providers.tsx
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "jotai";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </Provider>
+  );
+}
